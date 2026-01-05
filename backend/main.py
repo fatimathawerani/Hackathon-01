42cb4ccc4e8149d40e50a928c50fa7bc120b9239
fatimathawerani <fthawerani@gmail.com>
2026-01-06 01:41:46 +0500

    errors fixed

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,7 @@ load_dotenv()
 origins = [
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://127.00.1:8000",
+    "http://127.0.0.1:8000",
     "https://hackathon-01-eta.vercel.app"
 ]
 
